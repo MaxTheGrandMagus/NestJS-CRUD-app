42e1810014d72eb61a0ac1e2324cd1e27120439f
Max <veyronmclord@gmail.com>
2021-03-24 23:24:51 +0600

    pushing to heroku

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
